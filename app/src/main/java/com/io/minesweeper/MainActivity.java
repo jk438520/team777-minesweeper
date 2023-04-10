6f1ddc75bd1f2c3f59095893eb333d8ecdb206c3
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-10 20:33:39 +0200

    IS-8 Working prototype

--- app/src/main/java/com/io/minesweeper/MainActivity.java
+++ app/src/main/java/com/io/minesweeper/MainActivity.java
@@ -2,13 +2,59 @@ package com.io.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+
+        int numberOfColumns = 10;
+        int numberOfRows = 17;
+        int paddingDp = 20;
+
+        LinearLayout boardView = findViewById(R.id.boardView);
+
+        float scale = getResources().getDisplayMetrics().density;
+        int paddingPixel = (int) (paddingDp * scale + 0.5f);
+        int btnWeight  = (int) ((screenWidth - 2 * paddingPixel) / numberOfColumns);
+        int btnHeight = (int) ((screenWidth - 2 * paddingPixel) / numberOfColumns);
+
+        for (int row = 0; row < numberOfRows; row++) {
+            LinearLayout rowLayout = new LinearLayout(this);
+            rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+            rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            for (int column = 0; column < numberOfColumns; column++) {
+                ImageButton button = new ImageButton(this, null, 0, R.style.SquareStyle);
+                button.setId(row * numberOfRows + column);
+                button.setLayoutParams(new LinearLayout.LayoutParams(
+                        btnWeight,
+                        btnHeight
+                ));
+
+                rowLayout.addView(button);
+
+                Log.d("MainActivity", "Button " + row + " " + column + " created with id " + button.getId());
+            }
+
+            boardView.addView(rowLayout);
+        }
     }
 }
