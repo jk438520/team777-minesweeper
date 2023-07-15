466658bbfa755551f4c9f438732e4e4665c3d4eb
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-07-15 18:34:48 +0200

    Working get-set tests for UserModel

--- app/src/test/java/com/io/minesweeper/UserModelTest.java
+++ app/src/test/java/com/io/minesweeper/UserModelTest.java
@@ -4,11 +4,10 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Set;
 
 import static org.junit.Assert.*;
-
-import com.io.minesweeper.UserModel;
-
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,34 +18,47 @@ public class UserModelTest {
 
     @Test
     public void autoTestGettersSetters() {
-        Class aClass = UserModel.class;
+        Class<?> aClass = UserModel.class;
         Object instance;
-        try{
-            instance = aClass.getDeclaredConstructor().newInstance();
+        try {
+            Class<?>[] parameterTypes = new Class<?>[] { int.class, String.class };
+            Object[] parameters = new Object[] { 1, "test" };
+            instance = aClass.getDeclaredConstructor(parameterTypes).newInstance(parameters);
             Field[] declaredFields = aClass.getDeclaredFields();
-            for(Field f: declaredFields) {
+
+            for (Field f : declaredFields) if (!Modifier.isStatic(f.getModifiers())) {
                 String name = f.getName();
-                name = name.substring(0,1).toUpperCase() + name.substring(1);
+                name = name.substring(0, 1).toUpperCase() + name.substring(1);
                 String getterName = "get" + name;
                 String setterName = "set" + name;
                 Method getterMethod = aClass.getMethod(getterName);
                 Method setterMethod = aClass.getMethod(setterName, getterMethod.getReturnType());
+
                 // prepare a test value based on the filed type 
                 Object testVal = null;
                 Class<?> fType = f.getType();
-                if (fType.isAssignableFrom(Integer.class)) {
-                    testVal = 1234;
+                if (fType.isAssignableFrom(Integer.class) || fType.isAssignableFrom(int.class)) {
+                    testVal = Integer.valueOf("1234");
+                } else if (fType.isAssignableFrom(Long.class) || fType.isAssignableFrom(long.class)) {
+                    testVal = Long.valueOf("1234");
                 } else if (fType.isAssignableFrom(String.class)) {
                     testVal = "abcd";
-                } 
+                }
+
                 // test by composing the setter and getter
                 setterMethod.invoke(instance, testVal);
                 Object result = getterMethod.invoke(instance);
                 System.out.printf("Testing class=%s field=%s...\n", aClass.getName(), f.getName());
-                assertTrue(result.as("in class %s fields %s", aClass.getName(), f.getName()).isEqualTo(testVal));
-                }
-        } catch(Exception e) {
-           System.out.println(e.toString());
+                assertEquals("in class " + aClass.getName() + " fields " + f.getName(), result, testVal);
+            }
+
+            System.out.printf("Testing toString and equals of class=%s...\n", aClass.getName());
+            assertFalse("toString in class " + aClass.getName(), instance.toString().isBlank());
+
+            Set<Object> set = Set.of(instance);
+            assertTrue("equals n hashCode in class " + aClass.getName(), set.contains(instance));
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
     }
 }
