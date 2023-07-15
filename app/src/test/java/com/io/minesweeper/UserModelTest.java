f8bfe7ba98ba8184306b7b0104b60743b19930b5
jkolaczynski <j.kolaczynski@tensorflight.com>
2023-07-15 12:28:05 +0200

    change UserModel to UserModel.class

--- app/src/test/java/com/io/minesweeper/UserModelTest.java
+++ app/src/test/java/com/io/minesweeper/UserModelTest.java
@@ -19,31 +19,32 @@ public class UserModelTest {
 
     @Test
     public void autoTestGettersSetters() {
+        Class aClass = UserModel.class;
         Object instance;
         try{
-            instance = UserModel.getDeclaredConstructor().newInstance();
-            Field[] declaredFields = UserModel.getDeclaredFields();
+            instance = aClass.getDeclaredConstructor().newInstance();
+            Field[] declaredFields = aClass.getDeclaredFields();
             for(Field f: declaredFields) {
                 String name = f.getName();
-              name = name.substring(0,1).toUpperCase() + name.substring(1);
-              String getterName = "get" + name;
-              String setterName = "set" + name;
-              Method getterMethod = UserModel.getMethod(getterName);
-              Method setterMethod = UserModel.getMethod(setterName, getterMethod.getReturnType());
-              // prepare a test value based on the filed type 
-              Object testVal = null;
-              Class<?> fType = f.getType();
-              if (fType.isAssignableFrom(Integer.class)) {
-                  testVal = 1234;
-              } else if (fType.isAssignableFrom(String.class)) {
-                  testVal = "abcd";
-              }
-              // test by composing the setter and getter
-              setterMethod.invoke(instance, testVal);
-              Object result = getterMethod.invoke(instance);
-              System.out.printf("Testing class=%s field=%s...\n", UserModel.getName(), f.getName());
-              assertThat(result).as("in class %s fields %s", UserModel.getName(), f.getName()).isEqualTo(testVal);
-            }
+                name = name.substring(0,1).toUpperCase() + name.substring(1);
+                String getterName = "get" + name;
+                String setterName = "set" + name;
+                Method getterMethod = aClass.getMethod(getterName);
+                Method setterMethod = aClass.getMethod(setterName, getterMethod.getReturnType());
+                // prepare a test value based on the filed type 
+                Object testVal = null;
+                Class<?> fType = f.getType();
+                if (fType.isAssignableFrom(Integer.class)) {
+                    testVal = 1234;
+                } else if (fType.isAssignableFrom(String.class)) {
+                    testVal = "abcd";
+                } 
+                // test by composing the setter and getter
+                setterMethod.invoke(instance, testVal);
+                Object result = getterMethod.invoke(instance);
+                System.out.printf("Testing class=%s field=%s...\n", aClass.getName(), f.getName());
+                assertThat(result).as("in class %s fields %s", aClass.getName(), f.getName()).isEqualTo(testVal);
+                }
         } catch(Exception e) {
            System.out.println(e.toString());
         }
